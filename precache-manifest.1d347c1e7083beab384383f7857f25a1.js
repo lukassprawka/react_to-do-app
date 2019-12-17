da51d43b654ac55486752f4cdb659d9883e7c041
Lukasz Sprawka <lukas.sprawka@gmail.com>
2019-12-17 10:10:53 +0100

    Updates

---
+++ precache-manifest.1d347c1e7083beab384383f7857f25a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf3c2b8dd50ac2ba37f768acfe6323f4",
+    "url": "/react_to-do-app/index.html"
+  },
+  {
+    "revision": "72079c5475e90ca4aae2",
+    "url": "/react_to-do-app/static/css/main.2c41d8a7.chunk.css"
+  },
+  {
+    "revision": "717a52b61c1de84c6eb8",
+    "url": "/react_to-do-app/static/js/2.c19d1a11.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react_to-do-app/static/js/2.c19d1a11.chunk.js.LICENSE"
+  },
+  {
+    "revision": "72079c5475e90ca4aae2",
+    "url": "/react_to-do-app/static/js/main.c732bef1.chunk.js"
+  },
+  {
+    "revision": "e2211bc6534a10a50c6f",
+    "url": "/react_to-do-app/static/js/runtime-main.de229504.js"
+  }
+]);
